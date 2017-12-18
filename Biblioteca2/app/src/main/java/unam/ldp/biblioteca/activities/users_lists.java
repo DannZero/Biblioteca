7df5cf263d03e9518cd7bbdec4f289a58a8a0f8f
gerard6131@gmail.com <gerard6131@gmail.com>
2017-12-18 13:53:56 -0600

    Biblioteca2 guarda al usuario

---
+++ Biblioteca2/app/src/main/java/unam/ldp/biblioteca/activities/users_lists.java
@@ -0,0 +1,15 @@
+package unam.ldp.biblioteca.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import unam.ldp.biblioteca.R;
+
+public class users_lists extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users_lists);
+    }
+}
---
+++ Biblioteca2/app/src/main/java/unam/ldp/biblioteca/adapters/item_user_recycler.java
@@ -0,0 +1,15 @@
+package unam.ldp.biblioteca.adapters;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import unam.ldp.biblioteca.R;
+
+public class item_user_recycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_user_recycler);
+    }
+}
